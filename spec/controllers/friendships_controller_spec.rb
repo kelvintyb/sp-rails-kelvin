4c53b4a774eda9d8bd68a16224468d045a45b187
Kelvin Tan <kelvin.tyb@gmail.com>
2017-02-19 21:01:41 +0800

    story 3 tests

--- spec/controllers/friendships_controller_spec.rb
+++ spec/controllers/friendships_controller_spec.rb
@@ -32,14 +32,26 @@ RSpec.describe FriendshipsController, type: :controller do
 
   describe "GET #show_common" do
     it "shows emails of common friends between two Users" do
-      create_list(:user, 2)
-      @friends = []
-      @friends << User.first.email
-      @friends << User.last.email
+      create_list(:user, 3)
+      @first_user = User.first
+      @last_user = User.last
+      @common_friend = User.second
 
+      @friends = []
+      @friends << @first_user.email
+      @friends << @common_friend.email
       post :create, params: {friends: @friends}
-      expect(User.first.friend_users.first).to eq(User.last)
-      expect(User.last.friend_users.first).to eq(User.first)
+
+      @friends = []
+      @friends << @second_user.email
+      @friends << @common_friend.email
+      post :create, params: {friends: @friends}
+
+      @compare_friends = [@first_user.email, @second_user.email]
+      get :show_common, params: {friends: @compare_friends}
+      json = JSON.parse(response.body)
+      expect(json["friends"][0]).to eq(@common_friend.email)
+      expect(json["count"]).to eq(1)
     end
   end
 
