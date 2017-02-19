299cc7e215e79d1179866b88e9c37a783fa4312b
Kelvin Tan <kelvin.tyb@gmail.com>
2017-02-19 22:47:28 +0800

    minor changes

--- spec/controllers/blockades_controller_spec.rb
+++ spec/controllers/blockades_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe BlockadesController, type: :controller do
+  describe "POST #create" do
+    it "with 2 emails creates a blockade" do
+      create_list(:user, 2)
+      @first_user = User.first
+      @second_user = User.second
 
+      post :create, params: {requestor: @first_user, target: @second_user}
+      expect(User.first.blocking.first).to eq(User.second)
+      expect(User.second.blocked_by.first).to eq(User.first)
+    end
+  end
 end
